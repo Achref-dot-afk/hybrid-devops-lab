c7bd326faee777744864282b3543cffffb8b6ad4
Achref-dot-afk <achref.habli@etudiant-enit.utm.tn>
2025-08-27 00:26:08 +0100

    testing push to feature

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -2,3 +2,8 @@ resource "random_pet" "name" {
     length    = 2
     separator = "-"
 } // generate a random name for the resources
+
+resource "random_integer" "name" {
+  max = 9999
+  min = 1000
+}
