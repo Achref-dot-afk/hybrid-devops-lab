7a0d6b045c385e9dd40a15e660c8a303d8820a0e
Achref-dot-afk <achref.habli@etudiant-enit.utm.tn>
2025-08-26 16:05:20 +0100

    testing wif

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -1,4 +1,12 @@
-resource "random_pet" "name" {
-    length    = 2
-    separator = "-"
+resource "azurerm_storage_account" "example" {
+  name                     = "examplestorageacc"
+  resource_group_name      = "rg-test"
+  location                 = "eastus"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  network_rules {
+    default_action = "Allow"  # misconfiguration, should be "Deny" for private access
+    bypass         = ["AzureServices"]
+  }
 }
