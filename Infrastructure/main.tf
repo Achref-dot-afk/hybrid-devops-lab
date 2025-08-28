6a7e9582637056309277523b362bd7da821de2f0
Achref-dot-afk <achref.habli@etudiant-enit.utm.tn>
2025-08-28 17:11:28 +0100

    added jenkins vms and vpc

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -18,18 +18,4 @@ module "virtual_machines" {
     master_subnet_id = module.vpc.master_subnet_id
     worker_subnet_id = module.vpc.worker_subnet_id
 
-resource "random_pet" "name" {
-    length    = 2
-    separator = "-"
-} 
-
-resource "random_integer" "name" {
-  max = 9999
-  min = 1000
-}
-
-resource "random_pet" "f" {
-  length = 5
-  separator = "-"
-
 }
