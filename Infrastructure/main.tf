75afb453cde17d1e9af5ed0361f3574681cbd615
Achref-dot-afk <achref.habli@etudiant-enit.utm.tn>
2025-08-28 16:53:42 +0100

    added jenkins vms and vpc

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -1,4 +1,20 @@
-resource "random_pet" "name" {
-    length    = 2
-    separator = "-"
-} 
+data "azurerm_resource_group" "rg" {
+  name = "rg-terraform-resources"
+}
+
+module "vpc" {
+    source = "./modules/vpc"
+    environment = var.environment
+    location = var.location
+    rg_name = data.azurerm_resource_group.rg
+}
+
+module "virtual_machines" {
+    source = "./modules/virtual_machines"
+    environment = var.environment
+    location = var.location
+    rg_name = data.azurerm_resource_group.rg
+    master_subnet_id = module.vpc.master_subnet_id
+    worker_subnet_id = module.vpc.worker_subnet_id
+  
+}
---
+++ Infrastructure/modules/virtual_machines/jenkins-master.tf
@@ -0,0 +1,42 @@
+resource "azurerm_network_interface" "master-nic" {
+  name                = "master-nic"
+  location            = var.location
+  resource_group_name = var.rg_name
+
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = var.master_subnet_id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_virtual_machine" "master" {
+  name                  = "${var.environment}-jenkins-master"
+  location              = var.location
+  resource_group_name   = var.rg_name
+  network_interface_ids = [azurerm_network_interface.master-nic.id]
+  vm_size               = "Standard_DS1_v2"
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "myosdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = true
+    ssh_keys {
+        path     = "/home/azureuser/.ssh/authorized_keys"
+        key_data = var.ssh_key
+    }
+  }
+  tags = {
+    environment = var.environment
+  }
+}
