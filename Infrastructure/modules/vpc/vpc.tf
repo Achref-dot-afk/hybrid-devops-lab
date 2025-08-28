d43ea83f35a8197901f304a01ac85427a9a34674
Achref-dot-afk <achref.habli@etudiant-enit.utm.tn>
2025-08-28 17:04:28 +0100

    added jenkins vms and vpc

--- Infrastructure/modules/vpc/vpc.tf
+++ Infrastructure/modules/vpc/vpc.tf
@@ -1,5 +1,5 @@
 resource "azurerm_virtual_network" "customVNet" {
-  name = "${var.environment}-vnet"
+  name = "${var.environment}-Vnet"
   resource_group_name = var.rg_name
   location = var.location
   address_space = ["10.0.0.0/16"]
