8ebd50b05cd800f5ea911efef45c344bf6f6edc0
Achref-dot-afk <achref.habli@etudiant-enit.utm.tn>
2025-08-31 16:57:48 +0100

    added jenkins servers

--- Infrastructure/modules/virtual_machines/jenkins-master.tf
+++ Infrastructure/modules/virtual_machines/jenkins-master.tf
@@ -29,6 +29,10 @@ resource "azurerm_virtual_machine" "master" {
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
   }
+  os_profile {
+    computer_name = "jenkins-master"
+    admin_username = "jenkins_master"
+  }
   os_profile_linux_config {
     disable_password_authentication = true
     ssh_keys {
