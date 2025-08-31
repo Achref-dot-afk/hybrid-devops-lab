c00c9bee0190b11bad1a9db0e18a9e62e47f1f56
Achref-dot-afk <achref.habli@etudiant-enit.utm.tn>
2025-09-01 00:53:58 +0100

    modified destroy workflow

--- Infrastructure/modules/virtual_machines/public_ip.tf
+++ Infrastructure/modules/virtual_machines/public_ip.tf
@@ -2,7 +2,7 @@ resource "azurerm_public_ip" "example" {
   name                = "jenkins-master-ip"
   resource_group_name = var.rg_name
   location            = var.location
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
 
   tags = {
     environment = var.environment
